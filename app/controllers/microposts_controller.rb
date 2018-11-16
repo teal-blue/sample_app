75526e6ef3e7c10aa586fb27c263b548e47999e3
teal-blue <example@example.com>
2018-11-16 05:01:58 +0000

    返信機能を追加

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -3,16 +3,20 @@ class MicropostsController < ApplicationController
   before_action :correct_user,   only: :destroy
 
   def create
-    if (micropost_params.content.include("@"))
-      index_at = micropost_params.content.index("@")+1
-      index_space = micropost_params.content.index(" ",index_at) || micropost_params.content.count
-      reply = micropost_params.content.slice(index_at..index_space)
-      reply_user = User.find_by(name: reply)
-      if (!reply_user.nil)
-        micropost_params.in_reply_to = reply_user.id
+    param = micropost_params
+    con = param[:content]
+    if(con != "")
+      if (con.include?("@"))
+        index_at = con.index("@")+1
+        index_space = con.index(" ",index_at)-1 || con.count
+        reply = con.slice(index_at..index_space)
+        reply_user = User.find_by(name: reply)
+        if (!reply_user.nil?)
+          param[:in_reply_to] = reply_user.id
+        end
       end
     end
-    @micropost = current_user.microposts.build(micropost_params)
+    @micropost = current_user.microposts.build(param)
     if @micropost.save
       flash[:success] = "Micropost created!"
       redirect_to root_url
@@ -31,7 +35,7 @@ class MicropostsController < ApplicationController
   private
 
     def micropost_params
-      params.require(:micropost).permit(:content, :picture)
+      params.require(:micropost).permit(:content, :picture, :in_reply_to)
     end
 
     def correct_user
